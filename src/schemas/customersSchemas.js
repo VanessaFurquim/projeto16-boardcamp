6050dfe6d3c22694587b6bf338002fa43cfbaeff
Vanessa Furquim <vanessafurquim.contato@gmail.com>
2023-09-24 02:50:07 -0300

    Requirements: HUB partial evaluation 3: changing customersSchema to fix cpf format ([CLIENTES] errors)

--- src/schemas/customersSchemas.js
+++ src/schemas/customersSchemas.js
@@ -6,6 +6,6 @@ const joi = BaseJoi.extend(JoiDate)
 export const customersSchema = joi.object( {
     name: joi.string().required(),
     phone: joi.string().min(10).max(11).required(),
-    cpf: joi.string().length(11).alphanum().required(),
+    cpf: joi.string().pattern(/[0-9]{11}/).lengh(11).required(),
     birthday: joi.date().format("YYYY-MM-DD").required()
 })
